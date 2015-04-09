ad357beb300418d8d918ad3c6debbc9ff56f8e98
Daniel KIM <gwangpa@gmail.com>
2015-04-09 15:24:15 +1000

    no message

---
+++ Pod/Classes/MyLibrary.h
@@ -0,0 +1,13 @@
+//
+//  MyLibrary.h
+//  Pods
+//
+//  Created by Daniel KIM on 9/04/2015.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyLibrary : NSObject
+
+@end
---
+++ Pod/Classes/MyLibrary.m
@@ -0,0 +1,13 @@
+//
+//  MyLibrary.m
+//  Pods
+//
+//  Created by Daniel KIM on 9/04/2015.
+//
+//
+
+#import "MyLibrary.h"
+
+@implementation MyLibrary
+
+@end
